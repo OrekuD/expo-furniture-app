43bbd7937d581b23f513ab7cabc1a97be0e9a94b
unknown <opokudavid446@gmail.com>
2020-08-10 15:55:06 +0100

    Add dummy data

---
+++ data/products.ts
@@ -0,0 +1,78 @@
+export const products = [
+  {
+    id: Math.random().toString(),
+    name: "Ikea Armchair",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/ikea-ps-2012-armchair__1.webp"),
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/ikea-ps-2012-armchair__2.jpg"),
+      },
+      {
+        id: "2",
+        source: require("../assets/images/products/ikea-ps-2012-armchair__3.jpg"),
+      },
+    ],
+    price: 30,
+  },
+  {
+    id: Math.random().toString(),
+    name: "Janinge Chair",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/janinge-chair__1.webp"),
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/janinge-chair__2.webp"),
+      },
+    ],
+    price: 30,
+  },
+  {
+    id: Math.random().toString(),
+    name: "Urbeanears Headphones",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/urbanears_blue.png"),
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/urbanears_grey.png"),
+      },
+      {
+        id: "2",
+        source: require("../assets/images/products/urbanears_pink.png"),
+      },
+      {
+        id: "3",
+        source: require("../assets/images/products/urbanears_mint.png"),
+      },
+    ],
+    price: 80,
+  },
+  {
+    id: Math.random().toString(),
+    name: "Leifarne Armchair",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/leifarne-armchair__1.webp"),
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/leifarne-armchair__2.webp"),
+      },
+      {
+        id: "2",
+        source: require("../assets/images/products/leifarne-armchair__3.webp"),
+      },
+    ],
+    price: 30,
+  },
+];
