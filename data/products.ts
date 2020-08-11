023556eef0653a2b2b9a5cea012e8d6bb49a1a44
unknown <opokudavid446@gmail.com>
2020-08-11 10:08:13 +0100

    Add extra dummy products

--- data/products.ts
+++ data/products.ts
@@ -3,6 +3,25 @@ import { ProductObj } from "../types";
 export const products: Array<ProductObj> = [
   {
     id: Math.random().toString(),
+    name: "Fanbyn Bar Stool",
+    price: 249.99,
+    description:
+      "Aliquip irure nisi proident commodo sunt officia veniam incididunt. Lorem do reprehenderit aliquip duis incididunt. Consequat ex consectetur laborum labore consectetur adipisicing occaecat dolore eiusmod consequat voluptate veniam aliquip. Eiusmod excepteur amet elit non do mollit irure excepteur officia laboris. Commodo occaecat veniam consequat mollit do dolor deserunt magna nisi nulla sint eu anim cillum. Do ea est sint sint laboris eu officia veniam. Cupidatat amet fugiat incididunt occaecat sint eiusmod anim.",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/fanbyn-bar-stool-with-backrest__1.webp"),
+        color: "#E9E9E9",
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/fanbyn-bar-stool-with-backrest__2.webp"),
+        color: "#5E605F",
+      },
+    ],
+  },
+  {
+    id: Math.random().toString(),
     name: "Ikea Armchair",
     price: 249.99,
     description:
@@ -35,30 +54,49 @@ export const products: Array<ProductObj> = [
   },
   {
     id: Math.random().toString(),
-    name: "Urbeanears Headphones",
+    name: "Janinge Bar Stool",
+    price: 149.99,
+    description:
+      "Sit eiusmod eu amet dolore incididunt ea cillum fugiat in duis in sit. Laborum nisi nisi aliquip cupidatat amet occaecat mollit amet sit nisi veniam do cupidatat. Aute non nisi est reprehenderit amet consectetur consequat eu labore labore. Dolor enim ut reprehenderit velit elit Lorem irure esse officia exercitation enim. Commodo occaecat qui eiusmod aliquip est velit officia voluptate aute eiusmod tempor ad ex. Irure excepteur cupidatat ut eu adipisicing eu irure enim cillum consectetur.",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/janinge-bar-stool__1.webp"),
+        color: "#8C8C8C",
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/janinge-bar-stool__2.webp"),
+        color: "#E9EAE5",
+      },
+    ],
+  },
+  {
+    id: Math.random().toString(),
+    name: "Volfgang Chair",
     price: 89.99,
     description:
       "Esse culpa dolore anim irure. Amet labore laborum quis excepteur nulla fugiat reprehenderit dolore cillum laboris nostrud officia eiusmod. Est consectetur anim proident pariatur occaecat eiusmod sint quis. Est cillum incididunt consequat irure. Adipisicing dolore ea anim exercitation. Consequat minim nostrud aliqua cupidatat ullamco sit eu nisi aliquip aliquip aute. Minim sunt occaecat enim labore veniam incididunt aliquip adipisicing in eiusmod amet adipisicing.",
     images: [
       {
         id: "0",
-        source: require("../assets/images/products/urbanears_blue.png"),
-        color: "blue",
+        source: require("../assets/images/products/volfgang-chair__1.webp"),
+        color: "#B6B4B3",
       },
       {
         id: "1",
-        source: require("../assets/images/products/urbanears_grey.png"),
-        color: "grey",
+        source: require("../assets/images/products/volfgang-chair__2.webp"),
+        color: "#646466",
       },
       {
         id: "2",
-        source: require("../assets/images/products/urbanears_pink.png"),
-        color: "pink",
+        source: require("../assets/images/products/volfgang-chair__3.webp"),
+        color: "#CEC7BE",
       },
       {
         id: "3",
-        source: require("../assets/images/products/urbanears_mint.png"),
-        color: "#86A384",
+        source: require("../assets/images/products/volfgang-chair__4.webp"),
+        color: "#433F3E",
       },
     ],
   },
@@ -83,6 +121,107 @@ export const products: Array<ProductObj> = [
         id: "2",
         source: require("../assets/images/products/leifarne-armchair__3.webp"),
         color: "#B99C6F",
+      },
+    ],
+  },
+  {
+    id: Math.random().toString(),
+    name: "Adde chair",
+    price: 99.99,
+    description:
+      "Quis voluptate commodo consequat in nisi ad proident ea. Minim cupidatat qui amet in. Pariatur fugiat enim sunt id Lorem cupidatat. Tempor qui anim officia do irure consectetur cillum occaecat cillum. Sint ea sit proident Lorem id est amet amet occaecat eiusmod. Laboris adipisicing sint reprehenderit cupidatat velit veniam aliquip do incididunt minim proident. Elit tempor eiusmod reprehenderit mollit nostrud duis qui irure officia consequat tempor eiusmod.",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/adde-chair__1.webp"),
+        color: "#3B3C39",
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/adde-chair__2.webp"),
+        color: "#85898C",
+      },
+      {
+        id: "2",
+        source: require("../assets/images/products/adde-chair__3.webp"),
+        color: "#EDEBE7",
+      },
+    ],
+  },
+  {
+    id: Math.random().toString(),
+    name: "Terje Folding Chair",
+    price: 299.99,
+    description:
+      "Quis voluptate commodo consequat in nisi ad proident ea. Minim cupidatat qui amet in. Pariatur fugiat enim sunt id Lorem cupidatat. Tempor qui anim officia do irure consectetur cillum occaecat cillum. Sint ea sit proident Lorem id est amet amet occaecat eiusmod. Laboris adipisicing sint reprehenderit cupidatat velit veniam aliquip do incididunt minim proident. Elit tempor eiusmod reprehenderit mollit nostrud duis qui irure officia consequat tempor eiusmod.",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/terje-folding-chair__1.webp"),
+        color: "#262626",
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/terje-folding-chair__2.webp"),
+        color: "#CF9F76",
+      },
+      {
+        id: "2",
+        source: require("../assets/images/products/terje-folding-chair__3.webp"),
+        color: "#DADBD6",
+      },
+      {
+        id: "3",
+        source: require("../assets/images/products/terje-folding-chair__4.webp"),
+        color: "#262626",
+      },
+    ],
+  },
+  {
+    id: Math.random().toString(),
+    name: "Sofa",
+    price: 299.99,
+    description:
+      "Quis voluptate commodo consequat in nisi ad proident ea. Minim cupidatat qui amet in. Pariatur fugiat enim sunt id Lorem cupidatat. Tempor qui anim officia do irure consectetur cillum occaecat cillum. Sint ea sit proident Lorem id est amet amet occaecat eiusmod. Laboris adipisicing sint reprehenderit cupidatat velit veniam aliquip do incididunt minim proident. Elit tempor eiusmod reprehenderit mollit nostrud duis qui irure officia consequat tempor eiusmod.",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/sofa-1.png"),
+        color: "#202646",
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/sofa-2.png"),
+        color: "#918881",
+      },
+      {
+        id: "2",
+        source: require("../assets/images/products/sofa-3.png"),
+        color: "#B6B6B6",
+      },
+    ],
+  },
+  {
+    id: Math.random().toString(),
+    name: "Roenninge Chair Birch",
+    price: 299.99,
+    description:
+      "Quis voluptate commodo consequat in nisi ad proident ea. Minim cupidatat qui amet in. Pariatur fugiat enim sunt id Lorem cupidatat. Tempor qui anim officia do irure consectetur cillum occaecat cillum. Sint ea sit proident Lorem id est amet amet occaecat eiusmod. Laboris adipisicing sint reprehenderit cupidatat velit veniam aliquip do incididunt minim proident. Elit tempor eiusmod reprehenderit mollit nostrud duis qui irure officia consequat tempor eiusmod.",
+    images: [
+      {
+        id: "0",
+        source: require("../assets/images/products/roenninge-chair-birch__1.webp"),
+        color: "#E2C39E",
+      },
+      {
+        id: "1",
+        source: require("../assets/images/products/roenninge-chair-birch__2.webp"),
+        color: "#60735F",
+      },
+      {
+        id: "2",
+        source: require("../assets/images/products/roenninge-chair-birch__3.webp"),
+        color: "#2A2A28",
       },
     ],
   },
